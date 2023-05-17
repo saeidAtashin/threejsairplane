9d0b1bf7055ce48c82f8356304534783c4786b7c
saeidAtashin <105429979+saeidAtashin@users.noreply.github.com>
2023-05-17 15:19:24 +0430

    add loader

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,16 +1,25 @@
+import { useProgress } from "@react-three/drei"
+
 export const Overlay = () => {
+    const { progress } = useProgress()
     return (
         <div className="overlay">
-            <div className="intro">
-                <h1 className="logo">Atashin Air
-                    <div className="spinner">
-                        <div className="spinnner__image" />
-                    </div>
-                </h1>
-                <button className="explore">
-                    Explore
-                </button>
-            </div>
+            <div className={`loader ${progress === 100 ? "loader--disapear" : ""}`} />
+            {
+                progress === 100 && (
+                    <div className="intro">
+                    <h1 className="logo">Atashin Air
+                        <div className="spinner">
+                            <div className="spinnner__image" />
+                        </div>
+                    </h1>
+                    <button className="explore">
+                        Explore
+                    </button>
+                </div>
+                )
+            }
+         
         </div>
     )
 }
